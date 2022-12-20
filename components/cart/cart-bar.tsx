32ca492e5f0816cbf0f91cc3412de20904f10b94
Grzegorz Nęcka <strzypior@gmail.com>
2022-12-20 13:13:53 +0100

    create dev branch

--- components/cart/cart-bar.tsx
+++ components/cart/cart-bar.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import { useCartState } from "context/cart-context";
 import { ShoppingBagIcon } from "@heroicons/react/24/outline";
+import { apolloClient } from "graphQL/apolloClient";
 
 export default function CartBar() {
     const cartState = useCartState();
 
+    const handleOnClick = async () => {
+        await apolloClient.refetchQueries({
+            include: "active",
+        });
+    };
+
     return (
         <div>
+            <button onClick={handleOnClick}>refetch</button>
             <Link legacyBehavior href="/cart">
                 <a>
                     <div className=" flex">
