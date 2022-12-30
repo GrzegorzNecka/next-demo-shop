69fe50709baf75a1ea5a9eb48fae43bec11c82b5
Grzegorz Nęcka <strzypior@gmail.com>
2022-12-30 13:44:52 +0100

    fix: mdx

--- components/products/product-single-ui.tsx
+++ components/products/product-single-ui.tsx
@@ -1,194 +1,198 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
-import { NextSeo } from "next-seo";
-import type { ProductDetailsProps } from "./types";
-import ProductOption from "components/products/product-options";
-import { useCartState } from "context/cart-context";
-import Markdown from "components/markdown";
-import { ChangeEvent, useCallback, useEffect, useMemo, useState } from "react";
+import Image from 'next/image';
+import { NextSeo } from 'next-seo';
+import type { ProductDetailsProps } from './types';
+import ProductOption from 'components/products/product-options';
+import { useCartState } from 'context/cart-context';
+import Markdown from 'components/markdown';
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react';
 
 export const ProductSingleUI = ({ data }: ProductDetailsProps) => {
-    const cartState = useCartState();
+  const cartState = useCartState();
 
-    const isProductOpiotns = data?.option.length > 1 ? true : false;
+  const isProductOpiotns = data?.option.length > 1 ? true : false;
 
-    const [activeProductOptionId, setActiveProductOptionId] = useState<string>(data.option?.[0].id);
-    const [numberOfAvailableProductOptions, setNumberOfAvailableProductOptions] = useState<number>(0);
-    const [quantity, setQuantity] = useState<number>(1);
-    const resetQuantity = () => setQuantity(1);
+  const [activeProductOptionId, setActiveProductOptionId] = useState<string>(data.option?.[0].id);
+  const [numberOfAvailableProductOptions, setNumberOfAvailableProductOptions] = useState<number>(0);
+  const [quantity, setQuantity] = useState<number>(1);
+  const resetQuantity = () => setQuantity(1);
 
-    const findActiveProductOption = () => {
-        return data.option.filter((option) => {
-            if (option.id !== activeProductOptionId) {
-                return null;
-            }
+  const findActiveProductOption = () => {
+    return data.option.filter((option) => {
+      if (option.id !== activeProductOptionId) {
+        return null;
+      }
 
-            return option;
-        });
+      return option;
+    });
+  };
+
+  //todo  zamiast tego effectu możę lepiej zrobić z tego osobny komponent i dodać do niego useMomo na propsa
+  useEffect(() => {
+    //
+    const [activeProductOption] = findActiveProductOption();
+
+    if (!activeProductOption.total) {
+      return;
+    }
+
+    const cartItemOption = cartState.items.find((item) => {
+      return item.productOptionId === activeProductOptionId;
+    });
+
+    if (cartItemOption) {
+      setNumberOfAvailableProductOptions(activeProductOption.total - cartItemOption.quantity);
+      return;
+    }
+
+    setNumberOfAvailableProductOptions(activeProductOption.total);
+  }, [activeProductOptionId, cartState]);
+
+  const handleOnClick = () => {
+    const newCartItem = {
+      productOptionId: activeProductOptionId,
+      price: data.price,
+      title: data.title,
+      quantity,
+      imgUrl: data.thumbnailUrl,
+      slug: data.slug,
     };
 
-    //todo  zamiast tego effectu możę lepiej zrobić z tego osobny komponent i dodać do niego useMomo na propsa
-    useEffect(() => {
-        //
-        const [activeProductOption] = findActiveProductOption();
+    cartState.addItemToCart(newCartItem);
 
-        if (!activeProductOption.total) {
-            return;
-        }
+    //todo po pomyślnym dodaniu do koszyka zresetuj input
+    resetQuantity();
+  };
 
-        const cartItemOption = cartState.items.find((item) => {
-            return item.productOptionId === activeProductOptionId;
-        });
+  return (
+    <>
+      {/* // todo na tym elemencie też memoizacja ale z rozbiciem data przez Object.value() lub objec stringify i porównój wartości watrybutów */}
+      <SeoProvider data={data} />
 
-        if (cartItemOption) {
-            setNumberOfAvailableProductOptions(activeProductOption.total - cartItemOption.quantity);
-            return;
-        }
+      <div className="font-mono ">
+        <div className="grid grid-cols-2 gap">
+          <div>
+            <Image
+              src={data.thumbnailUrl}
+              alt={data.thumbnailAlt}
+              className=""
+              layout="responsive"
+              width={16}
+              height={9}
+              // objectFit="contain"
+              // objectPosition="center"
+            />
+          </div>
 
-        setNumberOfAvailableProductOptions(activeProductOption.total);
-    }, [activeProductOptionId, cartState]);
-
-    const handleOnClick = () => {
-        const newCartItem = {
-            productOptionId: activeProductOptionId,
-            price: data.price,
-            title: data.title,
-            quantity,
-            imgUrl: data.thumbnailUrl,
-            slug: data.slug,
-        };
-
-        cartState.addItemToCart(newCartItem);
-
-        //todo po pomyślnym dodaniu do koszyka zresetuj input
-        resetQuantity();
-    };
-
-    return (
-        <>
-            {/* // todo na tym elemencie też memoizacja ale z rozbiciem data przez Object.value() lub objec stringify i porównój wartości watrybutów */}
-            <SeoProvider data={data} />
-
-            <div className="font-mono ">
-                <div className="grid grid-cols-2 gap">
-                    <div>
-                        <Image
-                            src={data.thumbnailUrl}
-                            alt={data.thumbnailAlt}
-                            className=""
-                            layout="responsive"
-                            width={16}
-                            height={9}
-                            // objectFit="contain"
-                            // objectPosition="center"
-                        />
-                    </div>
-
-                    <div className="">
-                        <div className="flex justify-between pb-8">
-                            <h2 className="font-bold text-xl ">{data.title}</h2>
-                            <span className=" font-medium text-xl justify-self-end">{data.priceWithCurrency}</span>
-                            {/* <ProductArithmeticRating productSlug={data.slug} /> */}
-                        </div>
-                        {/* // product option */}
-
-                        {isProductOpiotns && (
-                            <ProductOption
-                                activeOption={activeProductOptionId}
-                                updateOption={setActiveProductOptionId}
-                                option={data.option}
-                            >
-                                warianty
-                            </ProductOption>
-                        )}
-
-                        <div>
-                            {numberOfAvailableProductOptions
-                                ? `total: ${numberOfAvailableProductOptions}`
-                                : "brak w magazynie"}
-                        </div>
-
-                        {/* <div>wybrano: {activeProductOption} </div> */}
-
-                        {cartState.isLoading ? (
-                            <div className="flex mb-8">
-                                {numberOfAvailableProductOptions ? (
-                                    <input
-                                        disabled
-                                        value={quantity}
-                                        type="number"
-                                        className="mb-0 w-1/4 mr-4 bg-transparent py-2 px-4 border-2 border-black rounded"
-                                    />
-                                ) : null}
-
-                                <button disabled className={`mb-0 w-3/4 text-blackfont-semibold btn-custom-primary`}>
-                                    dodawanie
-                                </button>
-                            </div>
-                        ) : (
-                            <div className="flex mb-8">
-                                {numberOfAvailableProductOptions ? (
-                                    <input
-                                        value={quantity}
-                                        onChange={(e) => setQuantity(Number(e.target.value))}
-                                        type="number"
-                                        className="mb-0 w-1/4 mr-4 bg-transparent py-2 px-4 border-2 border-black rounded"
-                                        max={numberOfAvailableProductOptions}
-                                        min={1}
-                                    />
-                                ) : null}
-
-                                {numberOfAvailableProductOptions ? (
-                                    <button
-                                        className={` mb-0 w-3/4 text-blackfont-semibold btn-custom-primary`}
-                                        onClick={handleOnClick}
-                                    >
-                                        dodaj do koszyka
-                                    </button>
-                                ) : null}
-                            </div>
-                        )}
-
-                        {/* <Print data={data.option} /> */}
-
-                        <article className="">{/* <Markdown>{data.longDescription}</Markdown> */}</article>
-                    </div>
-                </div>
-                <hr />
-                <div className="w-3/4 my-16 mx-auto">{/* <ProductReviewContainer productSlug={data.slug} /> */}</div>
+          <div className="">
+            <div className="flex justify-between pb-8">
+              <h2 className="font-bold text-xl ">{data.title}</h2>
+              <span className=" font-medium text-xl justify-self-end">
+                {data.priceWithCurrency}
+              </span>
+              {/* <ProductArithmeticRating productSlug={data.slug} /> */}
             </div>
-        </>
-    );
+            {/* // product option */}
+
+            {isProductOpiotns && (
+              <ProductOption
+                activeOption={activeProductOptionId}
+                updateOption={setActiveProductOptionId}
+                option={data.option}>
+                warianty
+              </ProductOption>
+            )}
+
+            <div>
+              {numberOfAvailableProductOptions
+                ? `total: ${numberOfAvailableProductOptions}`
+                : 'brak w magazynie'}
+            </div>
+
+            {/* <div>wybrano: {activeProductOption} </div> */}
+
+            {cartState.isLoading ? (
+              <div className="flex mb-8">
+                {numberOfAvailableProductOptions ? (
+                  <input
+                    disabled
+                    value={quantity}
+                    type="number"
+                    className="mb-0 w-1/4 mr-4 bg-transparent py-2 px-4 border-2 border-black rounded"
+                  />
+                ) : null}
+
+                <button
+                  disabled
+                  className={`mb-0 w-3/4 text-blackfont-semibold btn-custom-primary`}>
+                  dodawanie
+                </button>
+              </div>
+            ) : (
+              <div className="flex mb-8">
+                {numberOfAvailableProductOptions ? (
+                  <input
+                    value={quantity}
+                    onChange={(e) => setQuantity(Number(e.target.value))}
+                    type="number"
+                    className="mb-0 w-1/4 mr-4 bg-transparent py-2 px-4 border-2 border-black rounded"
+                    max={numberOfAvailableProductOptions}
+                    min={1}
+                  />
+                ) : null}
+
+                {numberOfAvailableProductOptions ? (
+                  <button
+                    className={`mb-0 w-3/4 text-blackfont-semibold btn-custom-primary`}
+                    onClick={handleOnClick}>
+                    dodaj do koszyka
+                  </button>
+                ) : null}
+              </div>
+            )}
+
+            {/* <Print data={data.option} /> */}
+
+            <article className="">{<Markdown>{data.longDescription}</Markdown>}</article>
+          </div>
+        </div>
+        <hr />
+        <div className="w-3/4 my-16 mx-auto">
+          {/* <ProductReviewContainer productSlug={data.slug} /> */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 // todo memoizazja na seo
 
 const SeoProvider = ({ data }: ProductDetailsProps) => {
-    return (
-        <>
-            <NextSeo
-                title={` produkt ${data.title}`}
-                description={`${data.description}`}
-                canonical={`https://naszsklep.vercel.app/products/${data.id}`}
-                openGraph={{
-                    url: `https://naszsklep.vercel.app/products/${data.id}`,
-                    title: data.title,
-                    description: data.description,
-                    images: [
-                        {
-                            url: data.thumbnailUrl,
-                            alt: data.thumbnailAlt,
-                            type: "image/jpeg",
-                        },
-                    ],
-                    site_name: "naszsklep",
-                }}
-                twitter={{
-                    handle: "@handle",
-                    site: "@site",
-                    cardType: "summary_large_image",
-                }}
-            />
-        </>
-    );
+  return (
+    <>
+      <NextSeo
+        title={` produkt ${data.title}`}
+        description={`${data.description}`}
+        canonical={`https://naszsklep.vercel.app/products/${data.id}`}
+        openGraph={{
+          url: `https://naszsklep.vercel.app/products/${data.id}`,
+          title: data.title,
+          description: data.description,
+          images: [
+            {
+              url: data.thumbnailUrl,
+              alt: data.thumbnailAlt,
+              type: 'image/jpeg',
+            },
+          ],
+          site_name: 'naszsklep',
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+      />
+    </>
+  );
 };
--- components/products/types.ts
+++ components/products/types.ts
@@ -1,35 +1,35 @@
-import { Option } from "graphQL/generated/graphql";
-import { MarkdownResult, ValueOf } from "types/types";
+import { Option } from 'graphQL/generated/graphql';
+import { MarkdownResult, ValueOf } from 'types/types';
 
-export type ProductOption = Pick<Option, "color" | "size" | "__typename" | "id" | "total">;
-export type ProductOptionList = Pick<Option, "id">;
+export type ProductOption = Pick<Option, 'color' | 'size' | '__typename' | 'id' | 'total'>;
+export type ProductOptionList = Pick<Option, 'id'>;
 
 export interface ProductProps {
-    readonly id: string;
-    readonly title: string;
-    readonly thumbnailUrl: string;
-    readonly thumbnailAlt: string;
-    readonly slug: string;
-    readonly price: number;
-    readonly priceWithCurrency: string;
-    // rating: number;
+  readonly id: string;
+  readonly title: string;
+  readonly thumbnailUrl: string;
+  readonly thumbnailAlt: string;
+  readonly slug: string;
+  readonly price: number;
+  readonly priceWithCurrency: string;
+  // rating: number;
 }
 
 export interface ProductDetails extends ProductProps {
-    // readonly longDescription: MarkdownResult;
-    readonly option: readonly ProductOption[];
-    readonly description: string;
+  readonly longDescription: MarkdownResult;
+  readonly option: readonly ProductOption[];
+  readonly description: string;
 }
 
 export interface ProductDetailsProps {
-    data: ProductDetails;
+  data: ProductDetails;
 }
 
 export interface ProductListItems extends ProductProps {
-    readonly option: readonly ProductOptionList[];
-    // rating: number;
+  readonly option: readonly ProductOptionList[];
+  // rating: number;
 }
 
 export interface ProductListItemProps {
-    data: ProductListItems;
+  data: ProductListItems;
 }
