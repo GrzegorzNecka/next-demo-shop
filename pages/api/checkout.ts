f1fdb3f438b0afdb175739a68e0e9a8b8bfbf0e3
Grzegorz Nęcka <strzypior@gmail.com>
2023-01-15 18:51:15 +0100

    feat/checkout: create api/chcekout, api/webkooks/stripe ,

---
+++ pages/api/checkout.ts
@@ -0,0 +1,78 @@
+import type {
+  GetProductBySlugQueryVariables,
+  GetProductDetailsBySlugQuery,
+} from 'graphQL/generated/graphql';
+import { GetProductBySlugDocument } from 'graphQL/generated/graphql';
+import { apolloClient } from 'graphQL/apolloClient';
+import type { NextApiHandler } from 'next/types';
+import Stripe from 'stripe';
+
+const checkoutHandler: NextApiHandler = async (req, res) => {
+  const stripeKey = process.env.STRIPE_SECRET_KEY;
+
+  if (!stripeKey) {
+    res.status(500).json({ message: 'Missing STRIPE_SECRET_KEY!' });
+    return;
+  }
+
+  const body = JSON.parse(req.body) as {
+    slug: string;
+    count: number;
+  }[];
+
+  const products = await Promise.all(
+    body.map(async (cartItem) => {
+      const product = await apolloClient.query<
+        GetProductDetailsBySlugQuery,
+        GetProductBySlugQueryVariables
+      >({
+        query: GetProductBySlugDocument,
+        variables: { slug: cartItem.slug },
+      });
+
+      return {
+        product,
+        count: cartItem.count,
+      };
+    }),
+  );
+
+  const line_items = products.map((product) => {
+    return {
+      adjustable_quantity: {
+        enabled: true,
+        minimum: 0,
+        maximum: product.count,
+      },
+      price_data: {
+        currency: 'PLN',
+        unit_amount: product.product.data.product!.price,
+        product_data: {
+          name: product.product.data.product!.name,
+          images: product.product.data.product!.images.map((i) => i.url),
+          metadata: {
+            slug: product.product.data.product!.slug,
+            id: product.product.data.product!.id,
+          },
+        },
+      },
+      quantity: product.count,
+    };
+  });
+
+  const stripe = new Stripe(stripeKey, { apiVersion: '2022-11-15' });
+
+  const stripeCheckoutSession = await stripe.checkout.sessions.create({
+    locale: 'pl',
+    payment_method_types: ['p24', 'card'],
+    success_url: `${process.env.NEXT_PUBLIC_HOST}/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_HOST}/checkout/cancel`,
+    line_items: line_items,
+    mode: 'payment',
+    // line_items: dev_line_Items,
+  });
+
+  res.status(201).json({ session: stripeCheckoutSession });
+};
+
+export default checkoutHandler;
---
+++ pages/api/webhooks/stripe.ts
@@ -0,0 +1,74 @@
+import type { NextApiHandler } from 'next/types';
+import Stripe from 'stripe';
+import { buffer } from 'micro';
+import type { StripeWebhookEvents } from 'stripeEvents';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const stripeWebhook: NextApiHandler = async (req, res) => {
+  const stripeKey = process.env.STRIPE_SECRET_KEY;
+  const StripeEndpointWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+  console.log(req.headers);
+  const buf = await buffer(req);
+  const sig = req.headers['stripe-signature'];
+
+  console.log('stripeKey', stripeKey);
+  console.log('sig', sig);
+  console.log('StripeEndpointWebhookSecret', StripeEndpointWebhookSecret);
+
+  if (!stripeKey || !StripeEndpointWebhookSecret || !sig) {
+    res.status(500).json({ message: 'Missing STRIPE_SECRET_KEY!' });
+    return;
+  }
+
+  const stripe = new Stripe(stripeKey, { apiVersion: '2022-11-15' });
+  // const event = req.body as StripeWebhookEvents;
+
+  try {
+    const event = stripe.webhooks.constructEvent(
+      buf,
+      sig,
+      StripeEndpointWebhookSecret,
+    ) as StripeWebhookEvents;
+    console.log('🚀 ~ file: stripe.ts:38 ~ conststripeWebhook:NextApiHandler= ~ event', event);
+
+    // switch (
+    //   event.type
+
+    //   case "payment_intent.succeeded":
+    //       const offerId = event.data.object.metadata.id;
+    //       console.log("🚀 offerId", offerId);
+    //       // @todo zaktualizuj zamówienie w GraphCms
+
+    //       return;
+    //   case "checkout.session.completed":
+    //       console.log("heckout.session.completed -status", event.type);
+    //       // @todo zaktualizuj zamówienie w GraphCms
+    //       return;
+    //   case "checkout.session.async_payment_failed":
+    //       console.log("checkout.session.async_payment_failed status", event.data.object.status);
+    //       // @todo zaktualizuj zamówienie w GraphCms
+    //       return;
+    //   case "checkout.session.async_payment_succeeded":
+    //       console.log("checkout.session.async_payment_succeeded status", event.data.object.status);
+    //       // @todo zaktualizuj zamówienie w GraphCms
+    //       return;
+    // )
+
+    res.json({ received: true });
+  } catch (err) {
+    let message = 'stripe webhook error';
+    if (err instanceof Error) {
+      message = err.message;
+    }
+
+    res.status(400).send(`Webhook Error: ${message}`);
+  }
+};
+
+export default stripeWebhook;
